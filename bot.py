0da27366d64efe574b5636d242c072568c39d48d
Francisco Cheda Pérez <franciscochedap@gmail.com>
2024-01-29 20:21:43 +0100

    Initial functionalities and mongo connector (#2)
    * initial tests
    
    * updated requirements
    
    * added parameter to select cli or bot
    
    * command with autocomplete
    
    * initial mongo queries
    
    * update
    
    * races and classes
    
    * initial character creation
    
    * first checks on create character
    
    * change race and class storage
    
    * finished character creation
    
    * fixed character creation ascis when the race has reduced asis due to a feat
    
    * christmas rewards

---
+++ bot.py
@@ -0,0 +1,220 @@
+# bot.py
+import os
+import sys
+import typing
+import discord
+from discord.ext import commands
+from discord import app_commands
+import random
+from dotenv import load_dotenv  #
+import mongo_connector
+
+
+connector = mongo_connector.mongo_connector()
+
+
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+GUILD = os.getenv("DISCORD_GUILD")
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+"""
+class CustomClient(discord.Client):
+    async def on_ready(self):
+        print(f"{self.user} has connected to Discord!")
+"""
+
+# bot command tests
+bot = commands.Bot(command_prefix="/", intents=intents)
+
+
+@bot.command(name="test")
+async def _test(ctx, arg1, arg2):
+    embed = discord.Embed(
+        title="Test command",
+        description=f" You sended:\n arg1: {arg1} \n arg2: {arg2} \n This is an embeded response",
+        color=0x00FF00,
+    )
+    embed.add_field(name="Esto es un field con titulo", value="Valor del field")
+    embed.add_field(name="Otro field", value="Valor del otro field")
+    await ctx.send(embed=embed)
+
+
+@bot.tree.command(name="tree_test")
+async def _tree_test(interaction: discord.Interaction):
+    await interaction.response.send_message(
+        f"Hello word, {interaction.user.guild}!", ephemeral=True
+    )
+
+
+@bot.tree.command(name="drink")
+async def drink(interaction: discord.Interaction, item: str):
+    await interaction.response.send_message(
+        f"{item}, {interaction.user}!", ephemeral=True
+    )
+
+
+@drink.autocomplete("item")
+async def drink_autocompletion(
+    interaction: discord.Interaction, current: str
+) -> typing.List[app_commands.Choice[str]]:
+    data = []
+    for drink_choice in ["beer", "blood", "tea", "coffe"]:
+        if current.lower() in drink_choice.lower():
+            data.append(app_commands.Choice(name=drink_choice, value=drink_choice))
+    return data
+
+
+@bot.event
+async def on_ready():
+    print("online")
+
+    try:
+        synced = await bot.tree.sync()
+        print(f"Synced {len(synced)} command(s)")
+    except Exception as e:
+        print(e)
+
+
+# bot general tests
+client = discord.Client(intents=intents)
+
+
+bot_channels = ["bot-test", "sorpresas-navideñas", "recompensas"]
+
+
+@client.event
+async def on_ready():
+    guild = discord.utils.get(client.guilds, name=GUILD)
+    print(
+        f"{client.user} is connected to the following guild:\n"
+        f"{guild.name}(id: {guild.id})"
+    )
+
+    # members = "\n - ".join([member.name for member in guild.members])
+    # print(f"Guild Members:\n - {members}")
+
+
+@client.event
+async def on_message(message):
+    print("got message")
+    print(f"Author: {message.author.name}")
+    print(f"Channel: {message.channel.name}")
+    print(f"Content: {message.content}")
+    print(f"Clean_Content: {message.clean_content}")
+    print(f"System_Content: {message.system_content}")
+
+    if message.channel.name not in bot_channels:
+        return
+
+    if message.author == client.user:
+        return
+
+    brooklyn_99_quotes = [
+        "I'm the human form of the 💯 emoji",
+        "Bingpot!",
+        (
+            "Cool. Cool cool cool cool cool cool cool, "
+            "no doubt no doubt no doubt no doubt."
+        ),
+    ]
+
+    sex_quotes = [
+        "Venga ponte, que te lo como todo",
+        "I want to f**k you so bad right now...",
+        "You would probably sleep better if we had sex.",
+        "I liked it so its mine UwU",
+    ]
+
+    if message.content == "99!":
+        response = random.choice(brooklyn_99_quotes)
+        await message.channel.send(response)
+    if message.content == "69!":
+        response = random.choice(sex_quotes)
+        await message.channel.send(response)
+
+    commands = [
+        "!get character ",
+        "!get player ",
+        "!update character ",
+        "!player character ",
+        "!insert log ",
+        "!get class ",
+        "!get race ",
+        "!get classes ",
+        "!get races ",
+        "!navidad ",
+    ]
+
+    if commands[0] in str(message.content):
+        # response = "character query: " + message.content[len(commands[0]) :]
+        response = connector.get_character(message.content[len(commands[0]) :])
+        await message.channel.send(response)
+    if commands[1] in str(message.content):
+        response = connector.get_player(message.content[len(commands[1]) :])
+        await message.channel.send(response)
+    if commands[2] in str(message.content):
+        response = connector.update_character(
+            message.content[len(commands[2]) :],
+        )
+        await message.channel.send(str(response))
+    if commands[3] in str(message.content):
+        response = connector.update_player(
+            message.content[len(commands[3]) :],
+        )
+        await message.channel.send(str(response))
+    if commands[4] in str(message.content):
+        response = connector.insert_log(
+            message.content[len(commands[4]) :],
+        )
+        await message.channel.send(str(response))
+    if commands[5] in str(message.content):
+        response = connector.get_class(
+            class_name=message.content[len(commands[5]) :],
+        )
+        await message.channel.send(str(response))
+    if commands[6] in str(message.content):
+        response = connector.get_race(
+            race_name=message.content[len(commands[6]) :],
+        )
+        await message.channel.send(str(response))
+    if commands[7] in str(message.content):
+        response = connector.get_races(message.content[len(commands[6]) :], "races")
+        await message.channel.send(str(response))
+    if commands[8] in str(message.content):
+        response = connector.get_race(message.content[len(commands[6]) :], "classes")
+        await message.channel.send(str(response))
+    if commands[9] in str(message.content):
+        response = connector.navidad(
+            message.content[len(commands[9]) :], message.author.name
+        )
+        await message.channel.send(str(response))
+    # if message.content == "Que piensas de Fedor?":
+    #    response = "Ufff esta buenorro el tio pero lo tiene pillado ems :("
+    #    await message.channel.send(response)
+
+
+# client.run(TOKEN)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "-cli":
+            print("starting as client")
+            client.run(TOKEN)
+
+        elif sys.argv[1] == "-bot":
+            print("starting as bot")
+            bot.run(TOKEN)
+    else:
+        print(
+            """
+      Usage
+      
+      missing command -bot or -cli
+      
+      """
+        )
---
+++ mongo_connector.py
@@ -0,0 +1,336 @@
+import os
+import pymongo
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import pprint
+import random
+
+load_dotenv()
+mongo_user = os.getenv("MONGO_USER")
+mongo_password = os.getenv("MONGO_PASSWORD")
+mongo_server = os.getenv("MONGO_SERVER")
+
+dbname = "d&d_server_main_db"
+character_collection = "test"
+player_collection = "test_player"
+logs_collection = "test_log"
+class_collection = "test_classes"
+race_collection = "test_races"
+
+
+class mongo_connector:
+    client = None
+
+    def __init__(self):
+        self.client = mongo_connector.get_connection()
+
+    def get_connection():
+        connection_str = f"mongodb+srv://{mongo_user}:{mongo_password}@{mongo_server}/?retryWrites=true&w=majority"
+        client = MongoClient(connection_str)
+        return client
+
+    def get_character(self, character_name: str = None):
+        print("character is ", character_name)
+        if character_name is not None:
+            return self.client[dbname][character_collection].find_one(
+                {"Personaje": character_name}
+            )
+        else:
+            return self.client[dbname][character_collection].find_one()
+
+    def get_player(self, player_name: str = None):
+        print("player is ", player_name)
+        if player_name is not None:
+            return self.client[dbname][player_collection].find_one(
+                {"Nombre": player_name}
+            )
+        else:
+            return self.client[dbname][player_collection].find_one()
+
+    def update_character(self, character_name=None, dict=None):
+        if character_name is not None:
+            if dict is not None:
+                newvalues = {"$set": dict}
+                self.client[dbname][character_collection].update_one(
+                    {"Personaje": character_name}, newvalues
+                )
+            else:
+                newvalues = {"$set": {"rabo": f"{random.randint(0,20)} cm"}}
+                self.client[dbname][character_collection].update_one(
+                    {"Personaje": character_name}, newvalues
+                )
+
+    def update_player(self, player_name=None, dict=None):
+        print(f"updating player {player_name}")
+        if player_name is not None:
+            if dict is not None:
+                newvalues = {"$set": dict}
+                result = self.client[dbname][player_collection].update_one(
+                    {"Nombre": player_name}, newvalues
+                )
+            else:
+                newvalues = {"$set": {"rabo": f"{random.randint(0,20)} cm"}}
+                result = self.client[dbname][player_collection].update_one(
+                    {"Nombre": player_name}, newvalues
+                )
+        return result
+
+    def insert_log(self, dict=None):
+        if dict is not None:
+            self.client[dbname][logs_collection].insert(dict)
+        else:
+            return None
+
+    def insert_character(self, dict=None):
+        if dict is not None:
+            return self.client[dbname][character_collection].insert(dict)
+        else:
+            return None
+
+    def get_classes(self, filter: dict = None, format: str = None):
+        cursor = self.client[dbname][class_collection].find(filter)
+        ret = cursor
+
+        if format == "lst":
+            ret = list(ret)
+        elif format == "class":
+            ret = list(set([item["class"] for item in list(ret)]))
+        elif format == "subclass":
+            ret = [item["subclass_name"] for item in list(ret)]
+
+        return ret
+
+    def get_races(self, filter: dict = None, format: str = None):
+        cursor = self.client[dbname][race_collection].find(filter)
+        ret = cursor
+        if format == "lst":
+            ret = list(ret)
+        elif format == "race":
+            ret = list(set([item["race"] for item in list(ret)]))
+        elif format == "subrace":
+            ret = [item["subrace_name"] for item in list(ret)]
+        return ret
+
+    def get_class(self, class_name: str = None, subclass_name: str = None):
+        if class_name is not None and subclass_name is not None:
+            return self.client[dbname][class_collection].find_one(
+                {"class": class_name, "subclass_name": subclass_name}
+            )
+        elif class_name is not None:
+            return self.client[dbname][class_collection].find_one(
+                {"class": class_name, "subclass_name": "None"}
+            )
+
+    def get_race(self, race_name: str = None, subrace_name: str = None):
+        print(f"race is {race_name} , subrace is {subrace_name}")
+        if race_name is not None and subrace_name is not None:
+            return self.client[dbname][race_collection].find_one(
+                {"race": race_name, "subrace_name": subrace_name}
+            )
+        elif race_name is not None:
+            return self.client[dbname][race_collection].find_one({"race": race_name})
+
+    def get_point_cost(self, asi):
+        point_cost = {
+            8: 0,
+            9: 1,
+            10: 2,
+            11: 3,
+            12: 3,
+            13: 5,
+            14: 7,
+            15: 9,
+        }
+        return point_cost.get(asi, None)
+
+    def navidad(self, character_name=None, autor=None):
+        cursor_player = self.client[dbname]["test_navidad"].find_one(
+            {"pj": character_name, "player": str(autor)}
+        )
+        if cursor_player is None:
+            return f"{str(autor)} no tiene ningun personaje llamado {character_name}"
+        # cursor_regalos = self.client[dbname]["test_navidad_regalos"].find(filter)
+        if cursor_player.get("regalo", None) is not None:
+            return "solo 1 regalo por cabeza"
+
+        print(type(cursor_player))
+        pprint.pprint(cursor_player)
+
+        regalo = ["nombre_regalo", "descripcion_regalo"]
+
+        roll = random.randint(1, 20)
+
+        if character_name == "Test":
+            roll = 25
+
+        cursor_regalos = self.client[dbname]["test_navidad_regalos"].find_one(
+            {"roll": roll}
+        )
+
+        if cursor_regalos is not None:
+            regalo[0] = cursor_regalos["item"]
+            regalo[1] = cursor_regalos["description"]
+        else:
+            return "Algo extraño ha pasado, regalo no encontrado"
+
+        print("updating char...")
+        self.client[dbname]["test_navidad"].update_one(
+            {"pj": character_name}, {"$set": {"regalo": regalo[0]}}
+        )
+
+        # return "hiden test"
+
+        print("Message autor is: " + str(autor))
+        result = f"{character_name} ha abierto su regalo y ha encontrado: {regalo[0]} !!! \n {regalo[1]}"
+        print(result)
+        return result
+
+    def check_point_buy(self, asis):
+        points = [self.get_point_cost(asi) for asi in asis]
+        if None not in points and sum(points) == 27:
+            return True
+        return False
+
+    def create_player(self, player_id: str):
+        pass
+
+    def create_character(
+        self,
+        discord_channel,
+        user_id,  # id discord
+        name,
+        race_name,
+        subrace_name,
+        class_name,
+        subclass_name,
+        _str,
+        _dex,
+        _con,
+        _int,
+        _wis,
+        _cha,
+        asi1,
+        asi2,
+        asi3=None,
+    ):
+        """example = connector.create_character("registros","test","pika2","Dragonborn","Metalic","Artificer","Alchemist",15,15,15,8,8,8,"str","dex")"""
+        valid_channels = ["registros", "bot-test"]
+        if discord_channel not in valid_channels:
+            return "channel is not valid"
+
+        db_race = self.get_race(race_name, subrace_name)
+        if db_race is not None:
+            print(f"selected raze is {db_race}")
+        else:
+            print("db_race is None")
+            return None
+
+        db_class = self.get_class(class_name, subclass_name)
+        if db_class is not None:
+            print(f"selected class is {db_class}")
+        else:
+            print("db_class is None")
+            return None
+
+        player = self.get_player(user_id)
+        if player is None:
+            self.create_player(user_id)
+            player = self.get_player(user_id)
+
+        if player.get("Current Pjs", 0) >= player.get("Max Pjs", 0):
+            return f"Max Pjs Reached, Current {player.get('Current Pjs',0)}, Max {player.get('Max Pjs',0)}"
+
+        exists = self.get_character(name)
+        if exists:
+            return "character name already exists"
+
+        if (
+            self.check_point_buy(
+                [
+                    _str,
+                    _dex,
+                    _con,
+                    _int,
+                    _wis,
+                    _cha,
+                ]
+            )
+            is False
+        ):
+            return (None, "Asis are invalid")
+        base_hp = db_class["hp_dice"] + db_class["hp_mod"] + db_race["hp_mod"]
+        _free_feats = 0
+        print(f"free feat is {db_race.get('free_feat',None)}")
+        if db_race.get("free_feat", None) is True:
+            print("adding free feat")
+            _free_feats = 1
+
+        if _free_feats == 0:
+            asi1val = 1 if asi3 is None else 2
+            asi2val = 1
+            asi3val = 0 if asi3 is None else 1
+        else:
+            asi1val = 1 if asi2 is None else 2
+            asi2val = 0 if asi2 is None else 1
+            asi3val = 0
+
+        for asi in [(asi1, asi1val), (asi2, asi2val), (asi3, asi3val)]:
+            target = None
+            if asi[0] == "str":
+                target = _str
+            if asi[0] == "dex":
+                target = _dex
+            if asi[0] == "con":
+                target = _con
+            if asi[0] == "int":
+                target = _int
+            if asi[0] == "wis":
+                target = _wis
+            if asi[0] == "cha":
+                target = _cha
+            if target is not None:
+                target += asi[1]
+
+        data = {
+            "Type": "Character",
+            "Personaje": name,
+            "Dueño": user_id,
+            "XP": 0,
+            "Fortuna": 0,
+            "Prestigio": 0,
+            "Evento": 0,
+            "Chikievento": 0,
+            "Descanso": 0,
+            "GP": 50,
+            "Level": 1,
+            "Clases": ({"class": class_name, "subclass": subclass_name, "lvl": 1}),
+            "Race": {"race": race_name, "subrace": subrace_name},
+            "HP": base_hp + (_con // 2 - 5),
+            "FUE": _str,
+            "DEX": _dex,
+            "CON": _con,
+            "INT": _int,
+            "WIS": _wis,
+            "CHA": _cha,
+            "FEATS": "",
+            "Unused Feats": _free_feats,
+            "Stats de master": False,
+            "Feat de Mastter": False,
+        }
+
+        result = self.insert_character(data)
+        result2 = self.update_player(
+            user_id, {"Current Pjs": player.get("Current Pjs", 0) + 1}
+        )
+
+        return (result, result2)  # TODO retornar None a la interfaz si funciono.
+
+
+if __name__ == "__main__":
+    print("testing mongo connection")
+    connector = mongo_connector()
+    db = connector.client["d&d_server_main_db"]
+    print(db)
+    collection = db["test"]
+    print(collection)
+    pprint.pprint(connector.get_character("Glaurimm"))
